350850a409d3d56a24349df5522a1bf3cabad53c
Twopants24 <loganlanc@icloud.com>
2026-03-28 17:32:54 +1100

    Add cache bust headers

--- src/main.js
+++ src/main.js
@@ -20,7 +20,7 @@ import {
   serializeState,
   unlockParcel,
   waterPlot,
-} from "./gameLogic.js?v=20260325-4";
+} from "./gameLogic.js?v=20260325-5";
 
 const STORAGE_KEY = "beanfarmer-save-v1";
 const CAMERA_BOUNDS = {
