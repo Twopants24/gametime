45a61df4a4f3338917e9e6e05b081066d060e9e8
Twopants24 <loganlanc@icloud.com>
2026-04-01 20:20:19 +1100

    Update cache busting versions

--- src/main.js
+++ src/main.js
@@ -20,7 +20,7 @@ import {
   serializeState,
   unlockParcel,
   waterPlot,
-} from "./gameLogic.js?v=20260401-2";
+} from "./gameLogic.js?v=20260401-3";
 
 const STORAGE_KEY = "beanfarmer-save-v1";
 const CAMERA_BOUNDS = {
