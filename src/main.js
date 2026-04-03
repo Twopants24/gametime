64ba1663d3dd23857d7f1ae0a15b5c5a425f4f76
Twopants24 <loganlanc@icloud.com>
2026-04-03 16:28:20 +1100

    Update bean farmer build info

--- src/main.js
+++ src/main.js
@@ -20,7 +20,7 @@ import {
   serializeState,
   unlockParcel,
   waterPlot,
-} from "./gameLogic.js?v=20260401-21";
+} from "./gameLogic.js?v=20260401-22";
 
 const STORAGE_KEY = "beanfarmer-save-v1";
 const ADMIN_KEY = "beanfarmer-admin-v1";
